6db9af46d6c99caf14310d1b931f54754fccafe4
AHMAD_HAKIMI AHMAD_RASIDI <hakimihakimi611@gmail.com>
2024-05-30 09:09:54 +0800

    Solved Bug UnknownHostException

--- discovery-server/src/main/java/org/microservice/v2/config/SecurityConfig.java
+++ discovery-server/src/main/java/org/microservice/v2/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig  {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeHttpRequests(auth0 -> auth0
+                .authorizeHttpRequests(auth0  -> auth0
                         .anyRequest().authenticated()
                 )
                 .httpBasic();
